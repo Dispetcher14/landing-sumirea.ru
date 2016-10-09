3fedebfc48fa50be1b8b1bbb52728815f81113d3
Ivan <anu2biz@yandex.ru>
2016-10-09 20:17:50 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(document).ready( function() {
+    $('.main-page').css('height' , window.innerHeight );
+    $('#trailer').css('height' , window.innerHeight );
+    $('.about-org').css('height' , window.innerHeight );
+});
