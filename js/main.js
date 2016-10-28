12c128b1984816adf13f58f02ff31531fdd7bc23
littleguga <fed777os@gmail.com>
2016-10-28 03:21:15 +0300

    update main.js from archive

--- js/main.js
+++ js/main.js
@@ -2,4 +2,6 @@ $(document).ready( function() {
     $('.main-page').css('height' , window.innerHeight );
     $('#trailer').css('height' , window.innerHeight );
     $('.about-org').css('height' , window.innerHeight );
+    $('.our-activity ').css('height' , window.innerHeight );
+    
 });
