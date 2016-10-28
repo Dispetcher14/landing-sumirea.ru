dbb417e883c8b766b382654d78e03225778666bb
littleguga <fed777os@gmail.com>
2016-10-29 01:50:16 +0300

    remove useless code

--- sources/js/main.js
+++ sources/js/main.js
@@ -1,7 +1,3 @@
 $(document).ready( function() {
-    $('.main-page').css('height' , window.innerHeight );
-    $('#trailer').css('height' , window.innerHeight );
-    $('.about-org').css('height' , window.innerHeight );
-    $('.our-activity ').css('height' , window.innerHeight );
     
 });
