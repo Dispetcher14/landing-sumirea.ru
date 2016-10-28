df053c2eee76cfdfc9ffd2d86a871b9ab7cbd27d
littleguga <fed777os@gmail.com>
2016-10-28 03:26:21 +0300

    refactor project structure

--- js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready( function() {
-    $('.main-page').css('height' , window.innerHeight );
-    $('#trailer').css('height' , window.innerHeight );
-    $('.about-org').css('height' , window.innerHeight );
-    $('.our-activity ').css('height' , window.innerHeight );
-    
-});
---
+++ sources/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready( function() {
+    $('.main-page').css('height' , window.innerHeight );
+    $('#trailer').css('height' , window.innerHeight );
+    $('.about-org').css('height' , window.innerHeight );
+    $('.our-activity ').css('height' , window.innerHeight );
+    
+});
