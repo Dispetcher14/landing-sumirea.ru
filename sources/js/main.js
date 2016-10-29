40bb5789049f3a769a030d3a2f508a2d774721f3
littleguga <fed777os@gmail.com>
2016-10-29 03:50:27 +0300

    add animation for scrolling

--- sources/js/main.js
+++ sources/js/main.js
@@ -1,3 +1,14 @@
-$(document).ready( function() {
-    
+$(document).ready(function () {
+    $(".scroll").on("click", function (e) {
+        //prevent default scrolling
+        e.preventDefault();
+
+        //get target element
+        var target = $(this).attr('href');
+
+        //scroll to it
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 700);
+    });
 });
